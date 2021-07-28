b5663311a59e28dae46d88570ea4dedf1657c9ba
Vertika Jain <vertika.jain@qed42.com>
2021-07-28 15:34:13 +0530

    View, Text, Button, TextInput tags, styles & useState()

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = useState('vertika')
+  const [age, setAge] = useState(5)
+
+  const pressHandler = () => {
+    setName('abc')
+    setPerson({ name: 'xyz', age: 13 })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.header}>
+        <Text>Name is <Text style={styles.boldText}>{name}</Text> !</Text>
+        <Text>Age is {age} </Text>
+      </View>
+      <TextInput
+        style={styles.input}
+        placeholder='e.g. Vertika'
+        onChangeText={(val) => setName(val)}
+        multiline>
+      </TextInput>
+      <TextInput
+        style={styles.input}
+        placeholder='e.g. 10'
+        onChangeText={(val) => setAge(val)}
+        keyboardType={'numeric'}>
+      </TextInput>
+
+      <View style={styles.buttonContainer}>
+        <Button title='update state' onPress={pressHandler} />
+      </View>
+
     </View>
   );
 }
@@ -18,4 +46,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: "pink",
+    padding: 20
+  },
+  boldText: {
+    fontWeight: 'bold'
+  },
+  body: {
+    backgroundColor: "yellow",
+    padding: 20,
+    alignItems: 'center'
+  },
+  buttonContainer: {
+    marginTop: 20
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'blue',
+    margin: 10,
+    padding: 8,
+    width: 200
+  }
 });
